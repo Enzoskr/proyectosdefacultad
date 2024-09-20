ec49988536829d314fcb11f57ce22c1f6083c42f
Enzoskr <escuderoenzochechu@gmail.com>
2024-09-20 20:15:08 -0300

    add ejercicios 1 a 5

---
+++ tp1.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <math.h>
+//ejercicio 1
+/* int main()
+{
+    char nombre[50], apellido[50];
+
+    printf("Ingrese su nombre: ");
+    scanf("%s", &nombre);
+    printf("Ingrese su apellido: ");
+    scanf("%s", &apellido);
+    printf("Hola %s %s\n", nombre, apellido);
+    
+    return 0;
+}*/
+
+//ejercicio 3
+
+/*int main ()
+{
+    int num1,num2, suma, resta, multiplicacion,division;
+
+    printf("Ingrese variable 1: ");
+    scanf("%d", &num1);
+    fflush(stdin);
+
+    printf("Ingrese variable 2: ");
+    scanf("%d", &num2);
+
+ suma = num1 + num2;
+ resta = num1 - num2;
+ multiplicacion = num1 * num2;
+ division = num1 / num2;
+
+    
+    printf("La suma de %d y %d es: %d\n", num1, num2, suma);
+    printf("La resta de %d y %d es: %d\n", num1, num2, resta);
+    printf("La multiplicacion de %d y %d es: %d\n", num1, num2, multiplicacion);
+    printf("La division de %d y %d es: %d\n", num1, num2, division);
+    
+    return 0;
+    } */
+
+//ejercicio 4
+
+/*
+int main()
+{
+    int num1, num2, operacion;
+
+    printf("Ingrese numero a: ");
+    scanf("%d", &num1);
+    printf("Ingrese numero b: ");
+    scanf("%d", &num2);
+    
+    operacion= (num1+num2)*(num1-num2);
+    printf("El resultado de la operacion (a+b)*(a-b) es: %d\n", operacion);
+
+
+}
+*/
+
+//ejercicio 5 
+//pow es para hacer este tipo de operaciones
+
+int main (){
+
+    int numbase, numexpo  ;
+    
+    printf("Ingrese la base: ");
+    scanf("%d", &numbase);
+    printf("Ingrese el exponente: ");
+    scanf("%d", &numexpo);
+    int resultado = (int)pow(numbase,numexpo);
+
+    printf("El resultado de %d elevado a la %d es: %d\n", numbase, numexpo, resultado);
+
+}
+
