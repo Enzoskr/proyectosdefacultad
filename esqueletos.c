f7a6819dc3cb613b1edd105d572a0675bceb9282
Enzoskr <escuderoenzochechu@gmail.com>
2024-10-13 23:33:24 -0300

    arreglo de carpetas y creacion de esqueleto

---
+++ esqueletos.c
@@ -0,0 +1,122 @@
+//librerias
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+//primera llamada a la funcion 
+//void fesqueleto(int hueso1, int hueso2, int hueso3);
+//esqueleto de un main con do-while
+/* int main(){
+    char seguir[3];
+    do
+    {
+        
+    printf("desea seguir? si-no\n");
+    scanf("%s", seguir);
+
+    } while (strcmp(seguir, "si") == 0);}
+*/
+
+//como hacerla a la funcion de esqueleto abajo
+/* 
+void fesqueleto(int hueso1, int hueso2, int hueso3);
+{
+    printf("El esqueleto tiene %d huesos, %d huesos y %d huesos\n", hueso1, hueso2, hueso3);
+}
+
+*/
+
+//esqueleto de sentencia if
+/* if (condicion)
+{
+    //codigo
+}
+else
+{
+    //codigo
+}
+*/
+
+//esqueleto de sentencia switch
+/* switch (variable)
+{
+case 1:
+    //codigo
+    break;
+case 2:
+    //codigo
+    break;
+default:
+    //codigo
+    break;
+}
+*/
+
+//esqueleto de sentencia for
+/* for (int i = 0; i < count; i++)
+{
+    //codigo
+}
+*/
+
+//esqueleto de sentencia while
+/* while (condition)
+{
+    //codigo
+}
+*/
+
+//esqueletos de scanf
+/* scanf("%d", &variable);
+scanf("%f", &variable);
+scanf("%c", &variable);
+scanf("%s", variable);
+*/
+
+//esqueletos de printf
+/* printf("El valor de la variable es: %d\n", variable);
+printf("El valor de la variable es: %f y %f\n", variable1, variable2);
+*/
+
+
+//esqueletos de funciones
+/*
+tipo_de_retorno nombre_funcion(tipo_de_dato parametro1, tipo_de_dato parametro2)
+{
+    //codigo
+    return valor_de_retorno;
+}
+*/
+
+
+// variables principales
+/* int numvuelo, tpovuelo, cantescalas, tpoescala, totaldevuelo, ban=1;
+char destino, escala, seguir[3];
+int contesqueleto1=0, contesqueleto2=0, contesqueleto3=0;
+float esqueleto1, esqueleto2, esqueleto3;
+ */
+
+//funcion void
+/* void fesqueleto(int hueso1, int hueso2, int hueso3)
+{
+    printf("El esqueleto tiene %d huesos, %d huesos y %d huesos\n", hueso1, hueso2, hueso3);
+}
+ */
+
+//funcion int
+/* int fesqueleto(int hueso1, int hueso2, int hueso3)
+{
+    return hueso1+hueso2+hueso3;
+}
+ */
+
+//funcion float
+/* float fesqueleto(float hueso1, float hueso2, float hueso3)
+{
+    return hueso1+hueso2+hueso3;
+}
+ */
+
+
+
